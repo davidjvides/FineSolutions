2a683d91895a0844d001b7805b17b4af3eb09ba1
davidjvides <70244058+davidjvides@users.noreply.github.com>
2020-10-26 21:01:29 -0500

    prueba 3

--- web/index.php
+++ web/index.php
@@ -32,7 +32,7 @@ $app->post('/enviodato', function (Request $request) use ($app) {
 
 // fue lo explicado en la primera clase del profesor , en cuestion de envios de datos 
 
-#$app->post('/quien', function (Request $request) use ($app) {
+$app->post('/quien', function (Request $request) use ($app) {
 #	$dbconn = pg_pconnect("host=ec2-34-235-62-201.compute-1.amazonaws.com port=5432 bdname=ddhgeqsn80f2cj user=ihbstqzjlnctoz password=b500e682b709fa43bf979f2296ed9091a6287ffb4dcefd7d6a51f469503bfc61")
 
 #	if ($dbconn) {
